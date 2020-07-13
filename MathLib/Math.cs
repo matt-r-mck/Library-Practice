a1a8fcfc8c68d8ebe0f7bd123fa7d7fee21eeec1
matt-r-mck <matt.r.mck@gmail.com>
2020-07-12 21:04:27 -0400

    Add project files.

---
+++ MathLib/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data;
+
+namespace MathLib {
+    public class Math {
+
+        public int Add(int a, int b) {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b) {
+            return a - b;
+        }
+
+    }
+
+}
---
+++ TestMathLib/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MathLib;
+
+namespace TestMathLib {
+    class Program {
+        static void Main(string[] args) {
+
+            var math = new MathLib.Math();
+            var answer = math.Add(1, 2);
+            Console.WriteLine($"{answer}");
+
+            answer = math.Subtract(11, 7);
+            Console.WriteLine($"{answer}");
+
+            Console.WriteLine(math);
+        }
+    }
+}
